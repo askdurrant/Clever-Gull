fb57b7c90457c08691f3b32c4701d1e9f5ccb8fb
Simon Bays <simon.bays@gmail.com>
2015-04-25 13:29:51 +0200

    added server service and header directive

---
+++ app/scripts/services/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name cleverGullApp.server
+ * @description
+ * # server
+ * Service in the cleverGullApp.
+ */
+angular.module('cleverGullApp')
+  .service('server', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
