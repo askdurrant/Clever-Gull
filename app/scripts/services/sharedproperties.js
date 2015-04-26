129b82a2ea484d3a74bc13d1ff243e06ac247f68
Alex <askdurrant@gmail.com>
2015-04-26 17:09:23 +0100

    Added files

---
+++ app/scripts/services/sharedproperties.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name cleverGullApp.sharedProperties
+ * @description
+ * # sharedProperties
+ * Service in the cleverGullApp.
+ */
+angular.module('cleverGullApp')
+  .service('sharedProperties', function () {
+  	var subreddits = [];
+  	return{
+  		addSubreddit: function(subreddit){
+  			subreddits.push(subreddit);
+  		}
+  	}
+
+});
