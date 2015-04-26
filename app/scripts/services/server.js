ab00bad68706d57a709c1dbf423ec747fd3ecf16
Simon Bays <simon.bays@gmail.com>
2015-04-26 16:47:57 +0200

    add searchsubreddit directive

--- app/scripts/services/server.js
+++ app/scripts/services/server.js
@@ -8,6 +8,14 @@
  * Service in the cleverGullApp.
  */
 angular.module('cleverGullApp')
-  .service('server', function () {
+  .service('server', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
+    return{
+    	getData: function(){
+    		return $http({
+    			url: "https://www.reddit.com/r/webdev.json",
+    			method: "GET"
+    		});
+    	}
+    }
   });
